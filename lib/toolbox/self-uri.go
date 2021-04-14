4c0cfbec834ed9ad076d59c0f350caa6106a12b8
Daniel Lamando <dan@danopia.net>
2021-04-14 21:23:16 +0200

    toolbox: New self-URI mechanism

--- lib/toolbox/self-uri.go
+++ lib/toolbox/self-uri.go
@@ -9,23 +9,29 @@ import (
 
 // Build cluster-wide absolute URI prefix to the current process
 func SelfURI(suffix string) (string, error) {
-	name, err := os.Hostname()
-	if err != nil {
-		log.Println("SelfURI Oops 1:", err)
-		return "", errors.New("SelfURI couldn't discover the OS hostname")
-	}
-
-	addrs, err := net.LookupHost(name)
-	if err != nil {
-		log.Println("SelfURI Oops 2:", err)
-		return "", errors.New("SelfURI couldn't resolve the OS hostname")
-	}
-
-	if len(addrs) < 1 {
+	selfIp := GetLocalIP()
+	if selfIp == "" {
 		log.Println("SelfURI Oops 2:", err)
 		return "", errors.New("SelfURI didn't locate any host IPs")
 	}
 
-	selfIp := addrs[0]
 	return "skylink+ws://" + selfIp + suffix, nil
+}
+
+// https://stackoverflow.com/a/31551220
+// GetLocalIP returns the non loopback local IP of the host
+func GetLocalIP() string {
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        return ""
+    }
+    for _, address := range addrs {
+        // check the address type and if it is not a loopback the display it
+        if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+            if ipnet.IP.To4() != nil {
+                return ipnet.IP.String()
+            }
+        }
+    }
+    return ""
 }
